c67f84f8045a942e3f5f203ae3ef0aa9f223150b
Serhii Shkliarskiy <shkliarskiy@gmail.com>
2022-09-11 18:15:53 +0300

    selenium done

---
+++ form_test.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+try: 
+    link = "http://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    required_block = browser.find_element(By.CLASS_NAME, "first_block")
+    
+    inputs = required_block.find_elements(By.TAG_NAME, "input")
+    
+    if len(inputs) != 3:
+        raise NoSuchElementException
+    
+    for input in inputs:
+        input.send_keys("bla-bla")
+           
+    time.sleep(5)
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
