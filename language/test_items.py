53d2688a06137f5310d72549f18ec5cef85e8094
Serhii Shkliarskiy <shkliarskiy@gmail.com>
2022-09-18 14:35:29 +0300

    language done

--- language/test_items.py
+++ language/test_items.py
@@ -1,1 +1,10 @@
-pass
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+def test_add_button(browser: webdriver.Chrome):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
+    
+    sleep(10)
