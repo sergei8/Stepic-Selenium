6b6c0e6ed73bacde9996360dacab3dcfe5baaef8
Serhii Shkliarskiy <shkliarskiy@gmail.com>
2022-09-22 20:35:26 +0300

    final login page done

---
+++ final/pages/locators.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    
+class LoginPageLocators():
+    LOGIN_FORM_ID = (By.ID, "login_form")
+    REGISTER_FORM_ID = (By.ID, "register_form")
+    
--- final/test_main_page.py
+++ final/test_main_page.py
@@ -2,15 +2,44 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
-
-def test_add_button(browser: webdriver.Chrome):
-    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
-    
-    # sleep(10)
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    
+def test_gest_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page_link = browser.current_url
+    login_page = LoginPage(browser, login_page_link)
+    login_page.should_be_login_form()
+    
+def test_gest_should_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page_link = browser.current_url
+    login_page = LoginPage(browser, login_page_link)
+    login_page.should_be_login_form()
+    
+def test_correct_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page_link = browser.current_url
+    login_page = LoginPage(browser, login_page_link)
+    login_page.should_be_login_url(login_page_link)
